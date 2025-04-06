bc315584becd41f1f1cdfa13f2386ab5269cfc1b
HsinTzuLai <lkj5123.vl@gmail.com>
2025-04-06 14:16:35 -0700

    brogramCardBuilt

---
+++ 2_brogram/src/components/Hero.jsx
@@ -0,0 +1,36 @@
+export default function Hero () {
+    return(
+        <>
+        <h5>Complete this training program if you want to ...</h5>
+        <ol className="benefit-list">
+            <li>Flow a simple program with guaranteed results</li>
+            <li>Get fit, healthy.strong and shredded</li>
+            <li>Learning more about gym, training and technique</li>
+            <li>Become a lifetime gym bro🧡</li>
+
+        </ol>
+        <h3>The Rules</h3>
+        <p>To complete this program, you <b>Must</b> follow these 3simple rules:</p>
+        <ul className="rules-list">
+            <div className="rule-item">
+                <p><b>Rest</b></p>
+                <p>Ensure that you takeing rest days where necessary</p>
+            </div>
+            <div className="rule-item">
+                <p><b>Reps</b></p>
+                <p>Every rep is a pause rep following a <abbr title="2 seconds down - 2 seconds pause - 2 seconds up">2 - 2 - 2 tempo</abbr></p>
+            </div>
+            <div className="rule-item">
+                <p><b>Weight</b></p>
+                <p>Select the maximum weight that allows you to complete the set with good form</p>
+            </div>
+        </ul>
+        <small>*The first and second set should be 75% and 85% of you working weight used for the last two sets.</small>
+        <h3>TheTraining Plan</h3>
+        <p>This training plan uses a structure know as the <b>Bro Split</b>, and follows this rotation ⬇️</p>
+        <p><b><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></b></p>
+        <p>Complete all of the workouts bellow and track your progress along the way ✅</p>
+
+        </>
+    )
+}
