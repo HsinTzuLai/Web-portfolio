f55fe8d89ef0e2430700734f8b104cd18de4ec61
HsinTzuLai <lkj5123.vl@gmail.com>
2025-04-06 23:00:06 -0700

    SelectedButton

--- 2_brogram/src/components/Grid.jsx
+++ 2_brogram/src/components/Grid.jsx
@@ -1,43 +1,45 @@
 import {workoutProgram as training_plan} from'../utils/index.js'
-import workoutCard from "./WorkoutCard.jsx"
+import WorkoutCard from "./WorkoutCard.jsx"
 
 export default function Grid() {
 
     const isLocked = true
-    const selectedWorkout = 0
+    const selectedWorkout = 4
 
     return(
         <div className="training-grid-plan">
             { Object.keys(training_plan).map((workout, workoutIndex) => {
-                const type = workoutIndex % 3 ===0 ? 
-                    'Push' : workoutIndex % 3 ===1 ? 
+                const type = workoutIndex % 3 === 0 ? 
+                    'Push' : workoutIndex % 3 === 1 ? 
                         'Pull' : 'Legs'
                 
                 const trainingPlan = training_plan[workoutIndex]
-                if (workoutIndex = selectedWorkout) {
+                const dayNum = ((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1
+                const icon = workoutIndex % 3 ===0 ? (
+                    <i className='fa-solid fa-dumbbell'></i>
+                ) : (
+                    workoutIndex % 3 ===1 ? (
+                        <i className='fa-solid fa-weight-hanging'></i>
+                    ):(
+                        <i className='fa-solid fa-bolt'></i>
+                    )
+                )
+
+
+                if (workoutIndex === selectedWorkout) {
                     return (
-                        <workoutCard key={workoutIndex}/>
+                        <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} icon={icon} dayNum={dayNum}/>
                     )
                 }
 
                 return (
                     <button className={'card plan-card ' + (isLocked ? 'inactive' : '')} key={workoutIndex}>
                         <div className='plan-card-header'>
-                            <p>Day {((workoutIndex/8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex+1 }</p>
+                            <p>Day {dayNum}</p>
                         </div>
                         {isLocked ? (
                             <i className='fa-solid fa-lock'></i>
-                        ) : (
-                            workoutIndex % 3 ===0 ? (
-                                <i className='fa-solid fa-dumbbell'></i>
-                            ) : (
-                                workoutIndex % 3 ===1 ? (
-                                    <i className='fa-solid fa-weight-hanging'></i>
-                                ):(
-                                    <i className='fa-solid fa-bolt'></i>
-                                )
-                            )
-                        )}
+                        ) : (icon)}
                         <div className='plan-card-header'>
                             <h4><b>{type}</b></h4>
                         </div>
--- 2_brogram/src/components/WorkoutCard.jsx
+++ 2_brogram/src/components/WorkoutCard.jsx
@@ -1,6 +1,70 @@
-export default function WorkoutCard() {
+import React from "react"
+export default function WorkoutCard(props) {
+    const { trainingPlan, workoutIndex, type, dayNum, icon} = props
+
+    const {warmup, workout} = trainingPlan || {}
+
     return(
-        <div>
+        <div className="workout-container">
+            <div className="workout-card card">
+                <div className="plan-card-header">
+                    <p>Day {dayNum}</p>
+                    {icon}
+                </div>
+                <div className="plan-card-header">
+                    <h2><b>{type} Workout</b></h2>
+                </div>
+            </div>
+            <div className="Warmup-grid">
+                <div className="exercise-name">
+                    <h4>Warmup</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+                {warmup.map((warmupExercise, warmupIndex) => {
+                    return (
+                        <React.Fragment key={warmupIndex}>
+                            <div className="exercise-name">
+                                <p>{warmupIndex + 1}. {warmupExercise.name}</p>
+                            </div>
+
+                            <p className="exercise-info">{warmupExercise.sets}</p>
+                            <p className="exercise-info">{warmupExercise.reps}</p>
+                            <input className="weight-input" placeholder="N/A" disabled/>
+
+                        </React.Fragment>
+                    )
+                })}
+            </div>
+
+            <div className="Workout-grid">
+                <div className="exercise-name">
+                    <h4>Workout</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+                {warmup.map((workoutExercise, workoutIndex) => {
+                    return (
+                        <React.Fragment key={workoutIndex}>
+                            <div className="exercise-name">
+                                <p>{workoutIndex + 1}. {workoutExercise.name}</p>
+                            </div>
+
+                            <p className="exercise-info">{workoutExercise.sets}</p>
+                            <p className="exercise-info">{workoutExercise.reps}</p>
+                            <input className="weight-input" placeholder="N/A" disabled/>
+
+                        </React.Fragment>
+                    )
+                })}
+            </div>
+
+            
+            
+
+
 
         </div>
     )
