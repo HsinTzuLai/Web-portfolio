bb68fbd6a59e55cfe549a8aeeef5bcc332c3c407
HsinTzuLai <lkj5123.vl@gmail.com>
2025-04-09 23:56:18 -0700

    brogramFinal

--- 2_brogram/src/components/Grid.jsx
+++ 2_brogram/src/components/Grid.jsx
@@ -1,14 +1,51 @@
+import { useState, useEffect } from "react";
 import {workoutProgram as training_plan} from'../utils/index.js'
 import WorkoutCard from "./WorkoutCard.jsx"
 
 export default function Grid() {
 
-    const isLocked = false
-    const selectedWorkout = 4
+    const [savedWorkouts,setSavedWorkouts] = useState(null)
+    const [selectedWorkout, setSelectedWorkout] = useState(null)
+    const completedWorkouts= Object.keys(savedWorkouts || {}).filter((val) => {
+        const entry = savedWorkouts[val]
+        return entry.isComplete
+    })
+
+    function handleSave(index,data) {
+        const newObj = {
+            ...savedWorkouts,
+            [index]:{
+                ...data,
+                isComplete: !!data.isComplete || !!savedWorkouts?.[index]?.iscomplete
+            }
+        }
+        setSavedWorkouts(newObj)
+        localStorage.setItem('brogram', JSON.stringify(newObj))
+        setSelectedWorkout(null)
+    }
+
+    function handleComplete(index, data) {
+        const newObj = {...data}
+        newObj.isComplete = true
+        handleSave(index,newObj)
+    }
+
+    useEffect(() => {
+        if(!localStorage) { return }
+        let savedData ={}
+        if(localStorage.getItem('brogram')) {
+            savedData = JSON.parse(localStorage.getItem('brogram'))
+        }
+        setSavedWorkouts(savedData)
+    },[])
 
     return(
         <div className="training-plan-grid">
             { Object.keys(training_plan).map((workout, workoutIndex) => {
+                const isLocked = workoutIndex === 0 ?
+                false:
+                !completedWorkouts.includes(`${workoutIndex - 1}`)
+
                 const type = workoutIndex % 3 === 0 ? 
                     'Push' : workoutIndex % 3 === 1 ? 
                         'Pull' : 'Legs'
@@ -28,12 +65,16 @@ export default function Grid() {
 
                 if (workoutIndex === selectedWorkout) {
                     return (
-                        <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} icon={icon} dayNum={dayNum}/>
+                        <WorkoutCard savedWeights={savedWorkouts ?.[workoutIndex]?.weights} 
+                        handleSave={handleSave} handleComplete={handleComplete} key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} icon={icon} dayNum={dayNum}/>
                     )
                 }
 
                 return (
-                    <button className={'card plan-card ' + (isLocked ? 'inactive' : '')} key={workoutIndex}>
+                    <button onClick={() => {
+                        if (isLocked) {return}
+                        setSelectedWorkout(workoutIndex)
+                    }} className={'card plan-card ' + (isLocked ? 'inactive' : '')} key={workoutIndex}>
                         <div className='plan-card-header'>
                             <p>Day {dayNum}</p>
                         </div>
